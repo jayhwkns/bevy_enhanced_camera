9363cfc74576273c3a15a6505006e0da5239a0e1
jayhwkns <hawkinsjr27@gmail.com>
2026-04-25 19:04:47 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Bevy Enhanced Camera
+
+A minimal configurable general purpose first person and third person camera
+library for Bevy.
+Natively uses [bevy_enhanced_input](https://github.com/simgine/bevy_enhanced_input).
---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "A flake using the nixpkgs rust toolchain wrapped with bevy-flake.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    bevy-flake = {
+      url = "github:swagtop/bevy-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { nixpkgs, bevy-flake, ... }:
+    let
+      bf = bevy-flake.configure { src = ./.; };
+    in
+    {
+      inherit (bf) packages formatter;
+
+      devShells = bf.forSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            name = "bevy-flake-nixpkgs";
+            packages = [
+              bf.packages.${system}.rust-toolchain
+              bf.packages.${system}.dioxus-cli
+              # bf.packages.${system}.bevy-cli
+            ];
+          };
+        }
+      );
+    };
+}
